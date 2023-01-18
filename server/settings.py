2cf30b84ef7cefabbf71559e45434b223212e6bc
Marcelo Custódio <custodios.marcelo@gmail.com>
2023-01-18 20:28:56 -0300

    added new database and report examples

--- server/settings.py
+++ server/settings.py
@@ -1,9 +1,12 @@
-MONGO_USER = "cafe-com-leite"
-MONGO_PASSWORD = "nw2A9rwx8QxmXmCRkZMrGF38pmuSWsDK"
+#MONGO_USER = "cafe-com-leite"
+#MONGO_PASSWORD = "nw2A9rwx8QxmXmCRkZMrGF38pmuSWsDK"
+MONGO_USER = "plantinha-inteligente"
+MONGO_PASSWORD = "beP1jlt9lXIGuFYj"
 """ 
 Usuário e senha do MongoDB do desafio
 
 ATENÇÃO: EM PRODUÇÃO OU TRABALHANDO COM DADOS VERÍDICOS, NUNCA COLOQUE SENHAS OU DADOS DE ACESSO DE BANCO DE DADOS NO CÓDIGO FONTE. Essas informações estão sendo salvas aqui apenas para fins educativos, uma vez que o banco de dados do desafio é público e não contém dados sensíveis.
 """
 
-MONGO_URL = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@sgh.6yzqa.mongodb.net/?readOnly=true"
+#MONGO_URL = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@sgh.6yzqa.mongodb.net/?readOnly=true"
+MONGO_URL = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@lpa-sgh-challenge.hd07rp6.mongodb.net/?readOnly=true" #?retryWrites=true&w=majority"
